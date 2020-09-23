9ccb7e527da354514dcd596931a87e857414d95a
Daniel Stahl <danny.stahl@gmail.com>
2020-09-23 14:35:46 -0700

    checking user input for errors

--- convertNumbers.cpp
+++ convertNumbers.cpp
@@ -1,5 +1,3 @@
-enum HexValues {A = 10, B = 11, C = 12, D = 13, E = 14, F = 15};
-
 #include "convertNumbers.hpp"
 
 int DecimalToBinary(int decimal) {
--- main.cpp
+++ main.cpp
@@ -10,24 +10,49 @@ int main() {
     int decimalNum;
     string hexNum;
     
-//    cout << "Convert decimal integer to binary and hex\n";
-//    cout << "Enter a positive decimal integer: ";
-//    cin >> decimalNum;
-//    
-//    cout << decimalNum << " is binary " << DecimalToBinary(decimalNum) << "\n";
-//    cout << decimalNum << " is Hex ";
-//    DecimalToHexadecimal(decimalNum);
-//    
-//    cout << "\nConvert a binary number to decimal integer\n";
-//    cout << "Enter a binary number: ";
-//    cin >> decimalNum;
-//    
-//    cout << decimalNum << " is decimal " << BinaryToDecimal(decimalNum);
+    cout << "Convert decimal integer to binary and hex\n";
+    cout << "Enter a positive decimal integer: ";
+    cin >> decimalNum;
+    
+    while (!cin || decimalNum < 0) {
+        cin.clear();
+        cin.ignore(200, '\n');
+        
+        cout << "Wrong input, please enter a positive number: ";
+        cin >> decimalNum;
+        
+    }
+    
+    cout << decimalNum << " is binary " << DecimalToBinary(decimalNum) << "\n";
+    cout << decimalNum << " is Hex ";
+    DecimalToHexadecimal(decimalNum);
+    
+    cout << "\nConvert a binary number to decimal integer\n";
+    cout << "Enter a binary number: ";
+    cin >> decimalNum;
+    
+    while (!cin || (decimalNum < 0 || decimalNum > 1)) {
+        cin.clear();
+        cin.ignore(200, '\n');
+        
+        cout << "Wrong input, please enter a binary number: ";
+        cin >> decimalNum;
+        
+    }
+    
+    cout << decimalNum << " is decimal " << BinaryToDecimal(decimalNum);
     
     cout << "\nConvert a hexadecimal number to decimal integer\n";
     cout << "Enter a hex number: ";
     cin >> hexNum;
     
-    cout << hexNum << " is decimal " << HexadecimalToDecimal(hexNum);
+    while (!cin) {
+        cin.clear();
+        cin.ignore(200, '\n');
+        
+        cout << "Wrong input, please enter a hex number: ";
+        cin >> decimalNum;
+    }
     
+    cout << hexNum << " is decimal " << HexadecimalToDecimal(hexNum);
 }
